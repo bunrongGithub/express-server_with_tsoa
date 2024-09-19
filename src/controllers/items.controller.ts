d368936bd167249b3fa491152d60403b1de3bd8f
Rong <bunbunrong993@gmail.com>
2024-09-19 14:53:22 +0700

    modyfy

--- src/controllers/items.controller.ts
+++ src/controllers/items.controller.ts
@@ -1,5 +1,5 @@
 /** Import from tsoa */
-import { Controller, Route ,Post, Body,Response, Middlewares, Get, Path, Put, Delete, Queries} from 'tsoa';
+import { Controller, Route, Post, Body, Response, Middlewares, Get, Path, Put, Delete, Queries } from 'tsoa';
 /** import items request from type */
 import { ItemCreateRequest, ItemGetAllRequest, ItemUpdateRequest } from '@/src/controllers/types/items-request.type';
 /** import response item to user from type */
@@ -11,67 +11,67 @@ import { itemCreateSchema } from '@/src/database/models/items.model';
 /** bast route of item */
 @Route("/v1/items")
 export class ItemController extends Controller {
-    @Post()
-    @Response(201,'Created Success')
-    @Middlewares(validateRequest(itemCreateSchema))
-    public async createItem(@Body() requestBody: ItemCreateRequest): Promise<ItemResponse>{
-      try{
-        const newProduct = await itemService.createItems(requestBody);
-        return {
-          message:"Success",
-          data:{
-            name: newProduct.name,
-            category: newProduct.category,
-            price: newProduct.price
-          }
+  @Post()
+  @Response(201, 'Created Success')
+  @Middlewares(validateRequest(itemCreateSchema))
+  public async createItem(@Body() requestBody: ItemCreateRequest): Promise<ItemResponse> {
+    try {
+      const newProduct = await itemService.createItems(requestBody);
+      return {
+        message: "Successfully",
+        data: {
+          name: newProduct.name,
+          category: newProduct.category,
+          price: newProduct.price
         }
-      }catch(error){
-        throw error;
       }
+    } catch (error) {
+      throw error;
     }
-    @Get('{id}')
-    public async getItemById(@Path() id: string): Promise<ItemResponse>{
-      try {
-        const item = await itemService.getItemById(id);
-        return {
-          message: "success",
-          data: item
-        };
-      } catch (error) {
-        throw error;
+  }
+  @Get('{id}')
+  public async getItemById(@Path() id: string): Promise<ItemResponse> {
+    try {
+      const item = await itemService.getItemById(id);
+      return {
+        message: "success",
+        data: item
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+  @Put('{id}')
+  public async updateItemById(@Path() id: string, @Body() requestBody: ItemUpdateRequest): Promise<ItemResponse> {
+    try {
+      const item = await itemService.updateItemById(id, requestBody);
+      return {
+        message: "success",
+        data: item
       }
+    } catch (error) {
+      throw error;
     }
-    @Put('{id}')
-    public async updateItemById(@Path() id: string, @Body() requestBody: ItemUpdateRequest): Promise<ItemResponse>{
-      try {
-        const item = await itemService.updateItemById(id,requestBody);
-        return {
-          message: "success",
-          data: item
-        }
-      } catch (error) {
-        throw error;
+  }
+  @Delete('{id}')
+  public async deleteItemById(@Path() id: string): Promise<void> {
+    try {
+      await itemService.deleteItemById(id);
+    } catch (error) {
+      throw error;
+    }
+  }
+  @Get()
+  public async getAllItems(@Queries() queries: ItemGetAllRequest): Promise<ItemPaginationResponse> {
+    try {
+      const response = await itemService.getAllItems(queries);
+      return {
+        message: "success",
+        data: response
       }
+    } catch (error) {
+      console.error(`ProductsController - getAllProducts() method error: ${error}`)
+      throw error;
     }
-    @Delete('{id}')
-    public async deleteItemById(@Path() id: string): Promise<void>{
-      try {
-        await itemService.deleteItemById(id);
-      } catch (error) {
-        throw error;
-      }
-    }
-    @Get()
-    public async getAllItems(@Queries() queries: ItemGetAllRequest): Promise<ItemPaginationResponse>{
-      try {
-        const response = await itemService.getAllItems(queries);
-        return {
-          message: "success",
-          data: response
-        }
-      } catch (error) {
-        console.error(`ProductsController - getAllProducts() method error: ${error}`)
-        throw error;
-      }
-    }
+  }
 }
